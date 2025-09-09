d6cdbf37627a5bdf8a5031c05ed3e9ef8b49ca84
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-09-09 16:13:46 +0200

    fix: remove replace in tests

--- core/tests/test_podman_kube_play.py
+++ core/tests/test_podman_kube_play.py
@@ -7,7 +7,6 @@ FIXTURES = Path(__file__).parent.joinpath("podman_fixtures", "kube_play")
 def test_podman_kube_play():
     basic = KubePlay(
         kube_play_file=FIXTURES.joinpath("basic", "play.yaml"),
-        replace=True,
     )
 
     with basic:
